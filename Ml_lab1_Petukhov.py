7f7c9735dcb6337d94e45865f19d0e0c30c03643
Tabaki59 <50752170+Tabaki59@users.noreply.github.com>
2020-03-31 01:08:55 +0500

    Make some progress in lab 1
    Намечены функции для рассчета показателей клиента, придуман цикл, TODO реализовать функции

--- Ml_lab1_Petukhov.py
+++ Ml_lab1_Petukhov.py
@@ -21,5 +21,30 @@ print(type(transact.RKDate[0]))
 transact = transact.sort_values("RKDate")
 print(transact.tail())
 
-rkdate = transact.RKDate[(transact.RKDate >= pd.to_datetime('2017-07-01')) & (transact.RKDate <= pd.to_datetime('2017-12-31'))]
-dates = transact.groupby(["CustomerID", rkdate]).count()['Restaurant']
+transact["visithappen"] = transact['RKDate'].between('2017-07-01','2017-12-31', inclusive=True)  
+transact_unic_id = transact.groupby('CustomerID')
+
+# Тестирую как играться с Groupby
+print(transact_unic_id.first())
+print(transact_unic_id.get_group(2728549)) 
+
+def get_recency(obj, cust_id):
+    #    Вот вообще не понимаю что тут считать есть только идея дата из задачи - дата первого визита но это просто потолок
+    print(obj.RKDate)
+    print('Да ты меня вызвал')
+    
+def get_frequency(obj, cust_id):
+    # Считаем среднее количество визитов в месяц дату 2017 07 01 - дата первого визита смотрим на разницу в месяцах
+    # Делим количество визитов на разницу в месяцах
+    # return количество
+    pass
+
+def get_monetary_value(obj, cust_id):
+    # средний чек клиента по всем покупкам до 2017-07-01
+    pass
+
+get_recency(transact_unic_id.get_group(2728549)) # Тест вызова функции (Кстати все ок работает)
+
+#for index, item in transact.iterrows():
+#    передаем в каждую функцию: transact_unic_id.get_group(item['CustomerID'])) и id клиента
+    
