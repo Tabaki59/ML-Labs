1cdd93a5c567d9b195466a56847b3da54e629f92
Tabaki59 <50752170+Tabaki59@users.noreply.github.com>
2020-04-01 17:38:45 +0500

    Lab 1 is done
    Выполнена первая лабораторная работа

--- Ml_lab1_Petukhov.py
+++ Ml_lab1_Petukhov.py
@@ -2,14 +2,14 @@
 """
 Created on Sun Feb  2 22:04:20 2020
 
-@author: User
+@author: Petukhov Alexandr Sergeevich https://github.com/Tabaki59/ML-Labs
 """
 # Задание 1
 import pandas as pd
 import numpy as np # надо для дат чтоб привести к месяцу
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 
 # Задание 1 импортируем файлы
 transact = pd.read_csv(r"G:\Учебка\Машинка\resto-asap2018\casc-resto-small.csv", sep = ",")
@@ -21,13 +21,13 @@ customers = customers.drop(columns=["ActivationDate","SubscribedEmail","Subscrib
 print(customers)
 print(customers.describe())
 
-# Задача 2 
+# Задача 2 считаем хар-ки клиента 
 transact.RKDate = pd.to_datetime(transact.RKDate) # Преобразуем дату из строки в ДАТУ
 print(type(transact.RKDate[0]))
 transact = transact.sort_values("RKDate") # Сортируем, пусть будет
 print(transact.tail())
 
-# Функция для расчета был ли визит (Если среди списка визитов клиента есть визит в указанном диапазоне ставим тру и едем дальше)
+# Функция для расчета был ли визит (Если среди списка визитов клиента есть визит в указанном диапазоне ставим тру и едем дальше иначе оставляем false)
 def get_visithappen(obj):
     result = False
     for index, item in obj.iterrows():
@@ -38,10 +38,10 @@ def get_visithappen(obj):
 
 # Функция для рассчета Recency
 def get_recency(obj):
-    #    Вот вообще не понимаю что тут считать есть только идея дата из задачи - дата последнего визита но это просто догадка, впрочем рабочая
+    #    Дата из задачи - дата последнего визита потом конвертим в дни, естественно смотрим на все визиты клиента чтоб найти последний
     df_with_correct_date = obj[obj['RKDate'] < pd.to_datetime("2017-07-01")]
     if not df_with_correct_date.empty:
-        result = pd.to_datetime("2017-07-01") - df_with_correct_date["RKDate"].max()
+        result = (pd.to_datetime("2017-07-01") - df_with_correct_date["RKDate"].max()) / np.timedelta64(1,'D')
     else:
         result = 0 
     return result
@@ -65,7 +65,7 @@ def get_frequency(obj):
 # Функция для рассчета Monetary Value 
 def get_monetary_value(obj):
     # средний чек клиента по всем покупкам до 2017-07-01
-    # Бежим фором по прилетевшему дата врейму, если дата меньше 2017 - 07 - 01 то суммируем чек, считаем чеки, потом делим одно на другое  
+    # Бежим фором по прилетевшему дата фрейму, если дата меньше 2017 - 07 - 01 то суммируем чек, считаем чеки, потом делим одно на другое  
     count = 0 
     mon_value = 0
     for index, item in obj.iterrows():
@@ -80,13 +80,6 @@ def get_monetary_value(obj):
     mon_value = 0
     return result     
 
-
-# Тестирую как играться с Groupby
-#print(transact_unic_id.first())
-#print(transact_unic_id.get_group(2728549)) 
-#get_recency(transact_unic_id.get_group(2728549)) # Тест вызова функции (Кстати все ок работает) Плюс тесты работы с пандас
-#print(get_monetary_value(transact_unic_id.get_group(2728549)))
-#print(transact_unic_id.get_group(2728549)[transact_unic_id.get_group(2728549)['RKDate'] < pd.to_datetime("2017-07-01")]) 
     
 transact_id = transact.groupby('CustomerID') # Группируем по id клиента
 
@@ -99,15 +92,28 @@ for index, item in transact.iterrows():  # Производим рассчет R
 transact = transact.drop_duplicates(subset=['CustomerID'])  # Сносим дубликаты по айдишникам чтоб каждый клиент и его хар-ки был представлен 1 раз
 transact = transact.drop(columns=["RKDate","RegionName","BrandsNames", "DishCategoryName", "Quantity", "SummBasic", "SummAfterPointsUsage"])  # Сносим все лишнее, что теперь будет только мешаться
 
-# Задание 3
+# Задание 3 притягиваем данные из второй таблицы
 customers.rename(columns={'CustomerId': 'CustomerID'}, inplace=True)    # переименовываем столбец ибо изначально они не совпадают 
 main_df = transact.merge(customers) # Лепим обе таблицы 
+main_df = main_df.dropna() # Сносим пустые значения так как пол содержат NaN
+main_df = main_df.replace({'Sex': {'Male': True,'Female': False}}) # Меняем полы на тип Boolean
 
-# Задание 4
+# Задание 4 делим выборку на тестовую и обучающую
 trg = main_df['Visithappen']  # Отделяем от выборки значение зависимой переменной
 trn = main_df.drop(columns=['Visithappen'], axis=1)
 Xtrn, Xtest, Ytrn, Ytest = train_test_split(trn, trg, test_size = 0.2) # Разбиваем выборку на тестовую и обучающую
 
-# Задание 5 
-logistic = LogisticRegression()
-logistic.fit(Xtrn,Ytrn)
+# Задание 5 обучение модели
+logistic = LogisticRegression(solver='lbfgs')
+logistic.fit(Xtrn,Ytrn) # Обучаем модель
+logistic.score(Xtrn, Ytrn) # На всякий случай производим оценку модели
+print('Coefficient: \n', logistic.coef_)
+print('Intercept: \n', logistic.intercept_)
+print('R² Value: \n', logistic.score(Xtrn, Ytrn))
+
+# Задание 6 предсказываем у
+predict = pd.Series(logistic.predict(Xtest)) # Предсказываем значения у и конвертим в сериес
+predict = predict.rename('Visithappen') # Проводим ренейм чтобы тестовый у и предсказанный хранились в одинаковом виде
+
+# Задание 7 считаем метрики
+report = classification_report(Ytest, predict) # Считаем метрики модели
